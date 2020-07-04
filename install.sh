afb771add7649cc198ff72c45cad71a021938281
Suzukihonoka <1787074172@qq.com>
2020-07-04 23:48:08 +0800

    add 88x2bu.ko and uncomment some code of the script.

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ echo "Usage: ./install.sh KVER"
 exit
 else
 cd $KVER
-#dpkg -i *deb
-#mv /boot/zImage /boot/zImage.old
-#cp ./Image /boot/zImage
+dpkg -i *deb
+mv /boot/zImage /boot/zImage.old
+cp ./Image /boot/zImage
 echo "Installation completed."
 echo "You may need to do a reboot for verity now."
 fi

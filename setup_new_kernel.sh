7ae30618c3c1de6f0783e01df2b5b3330774fbf0
starx <i@starx.ink>
2024-09-03 18:52:34 +0800

    CI: drop ramfs due to not enough ram

--- setup_new_kernel.sh
+++ setup_new_kernel.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-ROOT_DIR="/tmp/ramfs"
-BUILD_DIR="$ROOT_DIR/build"
+
+# Configurable vars
+BUILD_DIR="./build"
 KVERV="5.9.1"
 
+# Override by args if present
 if [ ! -z "$1" ]; then
-KVERV=$1
+  KVERV=$1
 fi
 
+if [ ! -z "$2" ]; then
+  BUILD_DIR=$2
+fi
+
+# Kernel archives info
 KVER="linux-$KVERV"
 FILE_NAME="$KVER.tar.xz"
 KURL="https://cdn.kernel.org/pub/linux/kernel/v${KVERV:0:1}.x"
 KDURL="$KURL/$FILE_NAME"
 
-mkdir -p $ROOT_DIR && sudo mount -t tmpfs -o size=10G tmpfs $ROOT_DIR
-mkdir -p $BUILD_DIR && cd $BUILD_DIR
+# Create the build dir if not present and enter
+mkdir -p "$BUILD_DIR" && cd "$BUILD_DIR"
 
-wget $KDURL
-tar -xf $FILE_NAME && rm $FILE_NAME
-cd "$BUILD_DIR/$KVER"
+# Download kernel archive
+wget "$KDURL"
+
+# Uncompress
+tar -xf "$FILE_NAME" && rm "$FILE_NAME"
+
+# Enter archive dir
+cd "$KVER"
